4d010d2c056bac3673b8412c7921f8faf8afd726
Zac Tiller <z.tiller99@gmail.com>
2023-11-11 17:15:45 +0000

    working with files

--- pages/api/assistantExample.ts
+++ pages/api/assistantExample.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import OpenAI from "openai";
+import fs from 'fs';
+import path from 'path';
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -11,14 +13,36 @@ export default async function handler(
   res: NextApiResponse
 ) {
   // assistant api: https://platform.openai.com/docs/assistants/overview?lang=node.js
-  // const availableModels = await openai.models.list();
 
-   // make an assistant
-   const assistant = await openai.beta.assistants.create({
-    name: "Math Tutor",
-    instructions: "You are a personal math tutor. Write and run code to answer math questions. Return me the answer in syntactitcally correct markdown",
+  const ms_file = path.join(process.cwd(), 'files', 'techstars_hackathon_markscheme.pdf')
+  // console.log('file', file);
+
+  // define our files:
+  const markscheme_file = await openai.files.create({
+    // @ts-ignore
+    file: fs.createReadStream(ms_file),
+    purpose: "assistants",
+  });
+
+  console.log('file test', markscheme_file);
+
+  // const paper_file = await openai.files.create({
+  //   file: fs.createReadStream('techstars_hackathon_questionpaper.pdf'),
+  //   purpose: "assistants",
+  // });
+
+  // const student_answer = await openai.files.create({
+  //   file: fs.createReadStream("mydata.csv"),
+  //   purpose: "assistants",
+  // });
+
+  // make an assistant
+  const assistant = await openai.beta.assistants.create({
+    name: "English Tutor",
+    instructions: "You are an English tutor. You are given two files which I want you to read. One will be a set of questions, the other a markscheme. Return me your answer in syntactitcally correct markdown",
     tools: [{"type": "retrieval"}],
-    model: "gpt-4-1106-preview"
+    model: "gpt-4-1106-preview",
+    // file_ids: [markscheme_file.id, paper_file.id]
   });
 
   // make a thread
@@ -27,7 +51,7 @@ export default async function handler(
   // pass in Q to existing thread
   await openai.beta.threads.messages.create(thread.id, {
     role: "user",
-    content: "I need to solve the equation `3x + 11 = 14`. Can you help me?",
+    content: "I want you to write model answers for me from the provided files. Return the answers in the form of: ((PROVIDED QUESTION)) : ((YOUR MODEL ANSWER FROM MARKSCHEME))",
   });
 
   // Use runs to wait for the assistant response and then retrieve it
@@ -58,4 +82,5 @@ export default async function handler(
     .pop();
 
   res.status(200).json({ data: lastMessageForRun });
+
 }
